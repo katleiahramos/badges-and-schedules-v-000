379edc1b1f8a85e6f2196c7876dbe9ef1f64b76d
Katleiah <katleiahramos@gmail.com>
2018-03-07 17:25:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,15 +11,15 @@ def batch_badge_creator(names)
   return badge_messages
 end
 
-def room_assignment(room)
-  return "You'll be assigned to room #{room}!"
-end
+#def room_assignment(room)
+#  return "You'll be assigned to room #{room}!"
+#end
 
 def assign_rooms(speakers_list)
   room = 1
   speaker_room = Array.new
   speakers_list.each do |speaker|
-    speaker_room.push(badge_maker(name) + room_assignment(room))
+    speaker_room.push()
     room += 1
   end
   return speaker_room
