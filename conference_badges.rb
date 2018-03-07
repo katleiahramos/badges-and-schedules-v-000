f0b77097254df000ad5ae17e77ddb7d488a20aaa
Katleiah <katleiahramos@gmail.com>
2018-03-07 17:29:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ def assign_rooms(speakers_list)
   return speaker_room
 end
 
-def printer(names, speakers_list) 
+def printer(names, speakers_list)
   puts batch_badge_creator(names)
   puts assign_rooms(speakers_list)
-  
+end 
